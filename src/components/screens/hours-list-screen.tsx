b7d70b947a4fa1311806a04c7e703bd7b267ae35
rgoles <renato.goles@gmail.com>
2025-09-15 21:25:37 +0200

    msg

--- src/components/screens/hours-list-screen.tsx
+++ src/components/screens/hours-list-screen.tsx
@@ -1,24 +1,42 @@
 import { useAuth } from "@/auth";
 import { db } from "@/config/firebase";
 import { collection, getDocs } from "firebase/firestore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const HoursListScreen = () => {
+  const [shifts, setShifts] = useState<any[]>([])
   const { user } = useAuth();
   const uid = user.uid;
 
   const fetchData = async () => {
+    if (!uid) return;
     const shiftsColRef = collection(db, "users", uid, "shifts");
     const querySnapshot = await getDocs(shiftsColRef);
-
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, "=>", doc.data());
-    });
+    const items = querySnapshot.docs.map((d) => ({ id: d.id, ...d.data() }));
+    setShifts(items);
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [uid]);
 
-  return <div>HoursListScreen</div>;
+  return (
+    <div>
+      <h2>HoursListScreen</h2>
+      {shifts.length === 0 ? (
+        <div>No shifts found.</div>
+      ) : (
+        <ul>
+          {shifts.map((shift: any) => (
+            <li key={shift.id}>
+              <pre>{JSON.stringify(shift.date, null, 2)}</pre>
+              <pre>{JSON.stringify(shift.shiftStart, null, 2)}</pre>
+              <pre>{JSON.stringify(shift.shiftEnd, null, 2)}</pre>
+              <pre>{JSON.stringify(shift.totalHours, null, 2)}</pre>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
