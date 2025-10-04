89e1af65fa249c23f1239e713d0c2886985e3136
rgoles <renato.goles@gmail.com>
2025-10-04 10:13:32 +0200

    changes to mobile nav

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,11 +79,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-<<<<<<< HEAD
-    "revision": "0.8u47stbonso"
-=======
-    "revision": "0.0eiahfoe35"
->>>>>>> a3d639e4d33c6bbcf25fa94972b86366a7780efa
+    "revision": "0.97e5vbkolfg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
