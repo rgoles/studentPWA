c9b44d6c19b4b051042f15299422e3c9312b3268
rgoles <renato.goles@gmail.com>
2025-11-30 19:10:09 +0100

    changes

--- src/components/atoms/custom-input.tsx
+++ src/components/atoms/custom-input.tsx
@@ -37,7 +37,7 @@ const Field = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         ref={ref}
         aria-invalid={!!error}
         className={cn(
-          "text-secondary-foreground focus:placeholder:text-muted-foreground/60 text-md block h-12 w-full rounded-md bg-neutral-100 px-3 pt-5 pb-2 font-medium outline-none placeholder:text-transparent focus:bg-white focus:ring-2 focus:ring-emerald-500",
+          "text-secondary-foreground focus:placeholder:text-muted-foreground/60 text-md block h-14 w-full rounded-md bg-neutral-100 px-3 pt-5 pb-2 font-medium outline-none placeholder:text-transparent focus:bg-white focus:ring-2 focus:ring-emerald-500",
           className,
         )}
         {...props}
@@ -54,7 +54,7 @@ function Label({ children, className }: React.ComponentProps<"label">) {
     <label
       htmlFor={id}
       className={cn(
-        "text-muted-foreground/70 group-[&:has(input:focus)]:text-ring pointer-events-none absolute top-3 left-2 text-base transition-all duration-100 ease-out group-[&:has(input:focus)]:top-1 group-[&:has(input:focus)]:scale-90 group-[&:has(input:focus)]:text-sm group-[&:has(input:not(:placeholder-shown))]:top-1 group-[&:has(input:not(:placeholder-shown))]:scale-90 group-[&:has(input:not(:placeholder-shown))]:text-sm",
+        "text-muted-foreground/70 group-[&:has(input:focus)]:text-ring pointer-events-none absolute top-4 left-2 text-base transition-all duration-100 ease-out group-[&:has(input:focus)]:top-1 group-[&:has(input:focus)]:scale-90 group-[&:has(input:focus)]:text-sm group-[&:has(input:not(:placeholder-shown))]:top-1 group-[&:has(input:not(:placeholder-shown))]:scale-90 group-[&:has(input:not(:placeholder-shown))]:text-sm",
         className,
       )}
     >
